fe65e0ee693cf612b5b0e565d976bdb5ff82d087
BReily89 <BrandonReily.br@gmail.com>
2017-10-10 09:26:37 -0400

    adding for homework

---
+++ react-hardware-store/src/components/Cart.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Cart extends Component {
+    render() {
+        return (
+            <div>
+                  render () {
+    return (
+      <div className="cart">
+        <h1>Cart</h1>
+        
+      </div>
+      
+    )
+  }
+}
+
+                
+            </div>
+        );
+    }
+}
+
+export default Cart;
